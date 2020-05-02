c9ebf846618ac9209ac5dc0de59883cde6e683c8
ingecraft <ingenious.craftsman@gmail.com>
2020-05-02 23:11:47 +0300

    Fixed typos

--- src/assets/game.js
+++ src/assets/game.js
@@ -1,29 +1,43 @@
+const question = require('./interface');
+
 class Game {
 	constructor (room, protagonist, actors) {
 		this.day = day;
 		this.protagonist = protagonist;
 		this.actors = actors;
-	}
-
-	new_turn() {
-		this.day.next_day()
-		this.describe_day()
-		this.describe_room()
-	}
-	describe_day() {
-		console.log(day.description());
-	}
-	
-	describe_room() {
-		console.log('This is what you see inside the room:\n')
-		this.actors.forEach(function(actor, index, array) {
-			console.log(actor.description);
-			console.log("\n");
+		this.dialogue = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout
 		})
 	}
 
-	take_action() {
+	newTurn() {
+		this.day.next_day();
+		this.describe_day();
+		this.describe_room();
 	}
+
+	describeDay() {
+		console.log(day.description());
+	}
+	
+	descibeRoom() {
+		console.log('This is what you see inside the room:\n');
+		this.actors.forEach(function(actor, index, array) {
+			console.log("{index}. {actor.description}\n");
+		});
+	}
+	
+	playerAction() {
+	}
+
+	chooseActor() {
+		this.dialogue.question('Choose actor')
+	}
+
+	chooseAction() {
+	}
+
 }
 
 module.exports = Game
