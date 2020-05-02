0dbfea6e7098f5e9ffafcde33a5747dbf803368e
ingecraft <ingenious.craftsman@gmail.com>
2020-05-02 23:56:36 +0300

    Added basic player functionality

--- src/assets/game.js
+++ src/assets/game.js
@@ -12,9 +12,10 @@ class Game {
 	}
 
 	newTurn() {
-		this.day.next_day();
-		this.describe_day();
-		this.describe_room();
+		this.day.nextDay();
+		this.describeDay();
+		this.describeRoom();
+		this.playerAction();
 	}
 
 	describeDay() {
@@ -23,18 +24,38 @@ class Game {
 	
 	descibeRoom() {
 		console.log('This is what you see inside the room:\n');
+		
 		this.actors.forEach(function(actor, index, array) {
 			console.log("{index}. {actor.description}\n");
 		});
 	}
 	
 	playerAction() {
+		var actor = this.chooseActor();		
+		var action = this.chooseAction();
+
+		switch(action) {
+			case 1:
+				this.protagonist.look(actor);
+				break;
+			case 2:
+				this.protagonist.use(actor);
+				break;
+			case 3:
+				this.protagonist.fight(actor);
+				break;
+			case 4:
+				this.protagonist.talk(actor);
+				break;
+			default:
+				console.log("You picked a wrong choice, fool... How hard can it be?")
+		}
 	}
 
 	chooseActor() {
 		var index;
-		var question = "Who do you want to interact with?"
-		var answer_text = "So this is your choice!"
+		var question = "Who do you want to interact with?";
+		var answer_text = "So this is your choice!";
 
 		question(question, answer_text).then(answer => {
 			index = answer;
@@ -54,7 +75,6 @@ class Game {
 
 		return index;
 	}
-
 }
 
 module.exports = Game
