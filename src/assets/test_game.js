ad755a9f8a0bb2d08193a647355ce95781d6472e
ingecraft <ingenious.craftsman@gmail.com>
2020-05-04 18:38:33 +0300

    Created test environment

--- src/assets/test_game.js
+++ src/assets/test_game.js
@@ -1,79 +1,134 @@
+/*-- Import Modules --*/
+const Protagonist = require('../entities/protagonist.js');
+const Actors = require('../entities/actors.js');
+
 const Game = require('./game');
 const Day = require('./day');
+/*-- Set Scene Variables --*/
+
+// Protagonist
+const protagonistName = "Joe";
+const protagonistPhysicalHealth = 100;
+const protagonistMentalHealth = 100;
+const protagonistPhysicalPower = 5;
+const protagonistMentalPower = 0;
+
+// Negative Object Chair
+const chairName = "uncomfortable chair";
+const chairPhysicalHealth = 15;
+const chairPhysicalPower = 2;
+const chairMentalPower = 5;
+const chairDesc = `an ${chairName} standing next to the desk`;
+const chairLookedResponse = `You see ${chairDesc}`;
+const chairUsedResponse = `You sit uncomfortably for the rest of the day and suffer a back pain. \nYou also feel utterly useless`;
+const chairTalkedResponse = `The ${chairName} does not respond to your desperate socializing attempt`;
+const chairFoughtResponse = `The ${chairName} resists your vicious attack and keeps its integrity`;
+const chairFoughtDestroyedResponse = `The ${chairName} loses the battle and lays broken into pieces`;
+
+// Positive Object Colored Paper
+const coloredPaperName = "colored paper";
+const coloredPaperPhysicalHealth = 1;
+const coloredPaperPhysicalPower = 1;
+const coloredPaperMentalPower = 10;
+const coloredPaperDesc = `a yellow ${coloredPaperName} laying on the floor`;
+const coloredPaperLookedResponse = `You see ${coloredPaperDesc}`;
+const coloredPaperUsedResponse = `You pick up the ${coloredPaperName} and start folding it in strange ways. \nTo your surprise it kind of looks like a deformed origami. \nYou feel a euphoric wave of accomplishment`;
+const coloredPaperTalkedResponse = `The ${coloredPaperName} will not answer to any calls from this particular dimension. \nThat makes you wonder... about physics... and stuff..`;
+const coloredPaperFoughtResponse = `The ${coloredPaperName} resists your irrational attack and keeps its integrity`;
+const coloredPaperFoughtDestroyedResponse = `The ${coloredPaperName} loses the battle and lays torn into pieces`;
+
+// Positive Creature Cat
+const catName = "grumpy cat";
+const catPhysicalHealth = 20;
+const catMentalHealth = 15;
+const catPhysicalPower = 20;
+const catMentalPower = 10;
+const catDesc = `a ${catName} with a judgmental look`;
+const catLookedResponse = `You see ${catDesc}`;
+const catUsedResponse = `You caress the ${catName} and your existence fills with serenity`;
+const catTalkedResponse = `The ${catName} seems to agree with what you imply`;
+const catFoughtResponse = `The ${catName} looks even grumpier now`;
+const catFoughtDestroyedResponse = `Powerless against your raging blows and unearthly screams, the ${catName} has a nervous breakdown and finally succumbs to its injuries`;
+
+// Negative Person Flatmate
+const flatmateName = "your flat mate";
+const flatmatePhysicalHealth = 15;
+const flatmateMentallHealth = 15;
+const flatmatePhysicalPower = 10;
+const flatmateMentalPower = 10;
+const flatmateDesc = `${flatmateName} sitting idle on the couch`;
+const flatmateLookedResponse = `You see ${flatmateDesc}.`;
+const flatmateUsedResponse = `You try to fist bump ${flatmateName} but he high fives you instead and then the whole scene becomes weird. \nYou feel weird`;
+const flatmateTalkedResponse = `Your flatmate does not seem to respond. You think maybe because he was born without ears, but you're not sure`;
+const flatmateFoughtResponse = `Your flatmate looks baffled by your aggressive move`;
+const flatmateFoughtDestroyedResponse = `Not able to confront your inexplicable furry, ${flatmateName} suffers a stroke and finally succumbs to his injuries`;
+
+/*-- Set Scene Instances --*/
+const protagonist = new Protagonist(
+    protagonistName, 
+    protagonistPhysicalHealth, 
+    protagonistMentalHealth, 
+    protagonistPhysicalPower,
+    protagonistMentalPower);
+
+const coloredPaper = new Actors.PositiveUseMentalObject(
+    coloredPaperName,
+    coloredPaperPhysicalHealth, 
+    coloredPaperPhysicalPower,
+    coloredPaperMentalPower,
+    coloredPaperDesc,
+    coloredPaperLookedResponse,
+    coloredPaperUsedResponse, 
+    coloredPaperTalkedResponse, 
+    coloredPaperFoughtResponse, 
+    coloredPaperFoughtDestroyedResponse);
+
+const chair = new Actors.NegativeUseObject(
+    chairName, 
+    chairPhysicalHealth, 
+    chairPhysicalPower,
+    chairMentalPower,
+    chairDesc,
+    chairLookedResponse,
+    chairUsedResponse, 
+    chairTalkedResponse, 
+    chairFoughtResponse, 
+    chairFoughtDestroyedResponse);
+
+const cat = new Actors.PositiveUseMentalCreature(
+    catName, 
+    catPhysicalHealth,
+    catMentalHealth,
+    catPhysicalPower,
+    catMentalPower,
+    catDesc,
+    catLookedResponse,
+    catUsedResponse, 
+    catTalkedResponse, 
+    catFoughtResponse, 
+    catFoughtDestroyedResponse);
+
+const flatmate = new Actors.NegativeUseMentalPerson(
+    flatmateName, 
+    flatmatePhysicalHealth, 
+    flatmateMentallHealth,
+    flatmatePhysicalPower,
+    flatmateMentalPower,
+    flatmateDesc,
+    flatmateLookedResponse,
+    flatmateUsedResponse, 
+    flatmateTalkedResponse, 
+    flatmateFoughtResponse, 
+    flatmateFoughtDestroyedResponse);
 
 
-class Actor {
-	constructor(description) {
-		this.description = description;
-	}
-	
-	looked() {
-		console.log('I was looked at');
-	}
-	
-	used() { 
-		console.log('I was used');
-	}
+let objects = [coloredPaper, chair]
 
-	talked() {
-		console.log('I was talked at');
-	}
 
-	fought() {
-		console.log('I am fightin back');
-	}
-}
+let persons = [flatmate];
 
-class Protagonist {
-	constructor(name) {
-		this.name = name;
-		this.alive = true;
-	}
-
-	use(actor) {
-		actor.used();
-	}
-
-	look(actor) {
-		actor.looked();
-	}
-
-	talk(actor) {
-		actor.talked();
-	}
-
-	fight(actor) {
-		actor.fought();
-		this.alive = false;
-	}
-	
-	isDead() {
-		return this.alive;
-	}
-}
-
-let chair = new Actor('Chair');
-let sofa = new Actor('Sofa');
-let table = new Actor('Table');
-let pickup = new Actor('Pickup');
-let lamp = new Actor('Lamp');
-
-let objects = [chair, table, sofa, pickup, lamp]
-
-let bill = new Actor('Bill');
-let john = new Actor('John');
-let bob = new Actor('Bob');
-let doug = new Actor('Doug');
-
-let persons = [bill, john, bob, doug];
-
-let dog = new Actor('Dog');
-let snake = new Actor('Snake');
-let spider = new Actor('Spider');
-let cat = new Actor('Cat');
-
-let creatures = [dog, snake, spider, cat];
-let initial = [dog, bill, chair]
+let creatures = [cat];
+let initial = [cat, flatmate, chair]
 
 let player = new Protagonist('Jimbo');
 let day = new Day(initial);
