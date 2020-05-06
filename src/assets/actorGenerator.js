5d6aa549ab27c9b9cc8ebcd4fa687a8896f88bbe
Philip Touchais <p.touchais@threenitas.com>
2020-05-06 17:24:32 +0300

    - changed actorGenerator name

---
+++ src/assets/actorGenerator.js
@@ -0,0 +1,36 @@
+class ActorGenerator{
+	constructor (objects, creatures, persons){
+		this.objects = objects;
+		this.creatures = creatures;
+		this.persons = persons;
+		// Defines how many actors are returned
+		this.how_many = 1;
+	}
+
+	newActors(){
+		var i;
+		var actors = [];
+		for (i = 0; i < this.how_many; i++) {
+			let actor = this.randomActor();
+			actors.push(actor);
+		}
+		return actors;
+	}
+
+	randomActor(){
+		var type = Math.round(Math.random() * 2)
+		switch(type) {
+			case 0:
+				var actor = this.objects[Math.floor(Math.random() * this.objects.length)];
+				return actor;
+			case 1:
+				var actor = this.creatures[Math.floor(Math.random() * this.creatures.length)];
+				return actor;
+			case 2:
+				var actor = this.persons[Math.floor(Math.random() * this.persons.length)];
+				return actor;
+		}
+	}
+}
+
+module.exports = ActorGenerator;
