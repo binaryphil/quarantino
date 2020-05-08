5511f6a4ef451fd585c0c2527069efa19c09e522
ingecraft <ingenious.craftsman@gmail.com>
2020-05-08 15:51:03 +0300

    Refactoring actor calss

--- src/entities/actor-refactor.js
+++ src/entities/actor-refactor.js
@@ -1,165 +1,158 @@
 class Actor {
-    constructor(name, description, lookedResponse, usedResponse, 
-				talkedResponse, foughtResponse, foughtDestroyedResponse) {
-        this.name = name;
-        this.description = description;
-        this.lookedResponse = lookedResponse;
-        this.usedResponse = usedResponse;
-        this.talkedResponse = talkedResponse;
-        this.foughtResponse = foughtResponse;
-        this.foughtDestroyedResponse = foughtDestroyedResponse;
-        this.lookCount = 0;
-        this.useCount = 0;
-        this.talkCount = 0;
-        this.fightCount = 0;
-        this.the = "the ";
-        this.actionAgainText1 = "";
-        this.actionAgainText2 = "";
-        this.seems = "";
-    }
+	constructor(name, description, lookedResponse, usedResponse, 
+		talkedResponse, foughtResponse, foughtDestroyedResponse) {
+		this.name = name;
+		this.description = description;
+		this.lookedResponse = lookedResponse;
+		this.usedResponse = usedResponse;
+		this.talkedResponse = talkedResponse;
+		this.foughtResponse = foughtResponse;
+		this.foughtDestroyedResponse = foughtDestroyedResponse;
+		this.lookCount = 0;
+		this.useCount = 0;
+		this.talkCount = 0;
+		this.fightCount = 0;
+		this.the = "the ";
+		this.actionAgainText1 = "";
+		this.actionAgainText2 = "";
+		this.seems = "";
+		this.lookIntro = `You choose to take a closer look at ${this.the}${this.name}${this.actionAgainText1}.`
+		this.talkIntro = `You choose to engage into conversation with ${this.the}${this.name}${this.actionAgainText1}.`
+		this.useIntro = `You choose to make use of ${this.the}${this.name}${this.actionAgainText1}.`
+		this.fightIntro = `You choose to fight ${this.the}${this.name}${this.actionAgainText1}.`
+	}
 
-    looked(protagonist) {
-        this.lookCount++;
-        const action = "Look at";
+	looked(protagonist) {
+		this.lookCount++;
+		const action = "Look at";
 
-        this.showAction(action, this.lookCount);
+		this.showAction(action, this.lookCount);
 
-        (this.lookCount > 1) ? this.setActionAgainText(): this.resetActionAgainText();
-    }
+		(this.lookCount > 1) ? this.setActionAgainText(): this.resetActionAgainText();
+	}
 
-    used(protagonist) {
-        this.useCount++;
-        const action = "Use";
+	used(protagonist) {
+		this.useCount++;
+		const action = "Use";
 
-        this.showAction(action, this.useCount);
+		this.showAction(action, this.useCount);
 
-        (this.useCount > 1) ? this.setActionAgainText(): this.resetActionAgainText();
-    }
+		(this.useCount > 1) ? this.setActionAgainText(): this.resetActionAgainText();
+	}
 
-    talked(protagonist) {
-        this.talkCount++;
-        const action = "Talk to";
+	talked(protagonist) {
+		this.talkCount++;
+		const action = "Talk to";
 
-        this.showAction(action, this.talkCount);
+		this.showAction(action, this.talkCount);
 
-        (this.talkCount > 1) ? this.setActionAgainText(): this.resetActionAgainText();
-    }
+		(this.talkCount > 1) ? this.setActionAgainText(): this.resetActionAgainText();
+	}
 
-    fought(protagonist) {
-        this.fightCount++;
-        const action = "Fight";
+	fought(protagonist) {
+		this.fightCount++;
+		const action = "Fight";
 
-        this.showAction(action, this.fightCount);
+		this.showAction(action, this.fightCount);
 
-        (this.fightCount > 1) ? this.setActionAgainText(): this.resetActionAgainText();
-    }
+		(this.fightCount > 1) ? this.setActionAgainText(): this.resetActionAgainText();
+	}
 
-    showAction(action, count) {
-        console.log(`[${action} ${this.the}${this.name} (${action} ${count})]`);
-    }
+	showAction(action, count) {
+		console.log(`[${action} ${this.the}${this.name} (${action} ${count})]`);
+	}
 
-    resetActionAgainText() {
-        this.actionAgainText1 = "";
-        this.actionAgainText2 = "";
-    }
-
-    setActionAgainText() {
-        this.actionAgainText1 = " once again";
-        this.actionAgainText2 = " once more";
-    }
+	resetActionAgainText() {
+		this.actionAgainText1 = "";
+		this.actionAgainText2 = "";
+	}
+	
+	setActionAgainText() {
+		this.actionAgainText1 = " once again";
+		this.actionAgainText2 = " once more";
+	}
 }
 
 
 class PhysicalBeing extends Actor {
     constructor(name, description, lookedResponse, usedResponse, talkedResponse, 
-				foughtResponse, foughtDestroyedResponse, physicalHealth=100, 
-				physicalPower=10) {
-		super(name, description, lookedResponse, usedResponse, talkedResponse, 
-			  foughtResponse, foughtDestroyedResponse);
+				        foughtResponse, foughtDestroyedResponse, physicalHealth=100, 
+				        physicalPower=10) {
+			super(name, description, lookedResponse, usedResponse, talkedResponse, 
+					  foughtResponse, foughtDestroyedResponse);
+			
+		  this.physicalHealth = physicalHealth;
+		  this.physicalPower = physicalPower;
+			this.the = '';
+	  }
 
-		this.physicalHealth = physicalHealth;
-		this.physicalPower = physicalPower;
-	}
+	  looked() {
+			super.looked();
+	  }
 
-	looked() {
-		super.looked();
-	}
+	  used() {
+		  super.used();
+	  }
 
-	used() {
-		super.used();
-	}
+	  talked() {
+		  super.talked();
+	  }
 
-	talked() {
-		super.talked();
-	}
+	  fought() {
+		  super.fought();
+			lose
+	  }
 
-	fought() {
-		super.fought();
-	}
+	  losePhysicalPower(loss) {
+			this.physicalPower -= loss;
+	  }
 
-    showAction(action, count) {
-		super.showAction();
-    }
-
-    resetActionAgainText() {
-		super.resetActionAgainText();
-    }
-
-    setActionAgainText() {
-		super.setActionAgainText();
-	}
-
-	losePhysicalPower(loss) {
-		this.physicalPower -= loss;
-	}
-
-	gainPhysicalPower(gain) {
-		this.physicalPower += gain;
-	}
-	
-	isAlive() {
-		return ((this.physicalHealth > 0) ? true: false);
+	  gainPhysicalPower(gain) {
+		  this.physicalPower += gain;
+	  }
 	  
-	}
+	  isAlive() {
+		  return ((this.physicalHealth > 0) ? true: false);
+	  }
 }
 
-class MentalBeing extends Actor {
-	constructor(name, description, lookedresponse, usedresponse, talkedresponse, 
-				foughtresponse, foughtdestroyedresponse, mentalHealth=100, 
-				mentalPower=10) {
-		super(name, description, lookedResponse, usedResponse, talkedResponse, 
-			  foughtResponse, foughtDestroyerdResponse, mentalHealth, mentalPower);
-		this.mentalHealth = mentalHealth;
-		this.mentalPower = physicalPower;
-	}
-	
-	looked() {
-		super.looked();
-	}
-
-	used() {
-		super.used();
-	}
-
-	talked() {
-		super.talked();
-	}
-
-	fought() {
-		super.fought();
-	}
-
-	loseMentalPower(loss) {
-		this.mentalPower -= damage;
-	}
-
-	gainMentalPower(gain) {
-		this.mentalPower += gain;
-	}
-	
-	isAlive() {
-		return ((this.mentalHealth > 0) ? true: false);
-	}	
-}
-
+// class MentalBeing extends Actor {
+// 	constructor(name, description, lookedresponse, usedresponse, talkedresponse, 
+// 				foughtresponse, foughtdestroyedresponse, mentalHealth=100, 
+// 				mentalPower=10) {
+// 		super(name, description, lookedResponse, usedResponse, talkedResponse, 
+// 			  foughtResponse, foughtDestroyerdResponse, mentalHealth, mentalPower);
+// 		this.mentalHealth = mentalHealth;
+// 		this.mentalPower = physicalPower;
+// 	}
+// 	
+// 	looked() {
+// 		super.looked();
+// 	}
+// 
+// 	used() {
+// 		super.used();
+// 	}
+// 
+// 	talked() {
+// 		super.talked();
+// 	}
+// 
+// 	fought() {
+// 		super.fought();
+// 	}
+// 
+// 	loseMentalPower(loss) {
+// 		this.mentalPower -= damage;
+// 	}
+// 
+// 	gainMentalPower(gain) {
+// 		this.mentalPower += gain;
+// 	}
+// 	
+// 	isAlive() {
+// 		return ((this.mentalHealth > 0) ? true: false);
+// 	}	
+// }
+// 
 module.exports = PhysicalBeing
--- src/entities/test_actors.js
+++ src/entities/test_actors.js
@@ -5,9 +5,13 @@ const description = 'A handosome man';
 const lookResponse = 'You see a handsome man';
 const usedResponse = 'A handsome man like him cannot be used';
 const talkResponse = 'You dont have the guts to talk to such a handsome man';
-const fightResponse = 'Dont even think about it';
+const foughtResponse = 'Dont even think about it';
+const foughtDestroyedResponse = 'You destroyed a handsome man. Or not?';
 
-var human = new PhysicalBeing(description, lookResponse, usedResponse, talkResponse, fightResponse);
+var human = new PhysicalBeing(name, description, lookResponse, usedResponse, talkResponse, foughtResponse,
+				                      foughtDestroyedResponse);
 console.log(human.isAlive());
 console.log(human.looked());
+console.log(human.used());
+console.log(human.talked());
 console.log(human.fought('protagonist'));
