25e1f6dd5d1f23cc90611ab9fafe98cfc39bbbd6
ingecraft <ingenious.craftsman@gmail.com>
2020-05-22 18:21:32 +0300

    Added randon generation methods for random actor

--- src/assets/environment.js
+++ src/assets/environment.js
@@ -1,21 +1,45 @@
-class Evnironment {
-  constructor(entities) {
-    this.entities = entities;
+class Environment {
+  constructor(entitiesPool) {
+    this.entitiesPool = entitiesPool;
+    this.entityTypes = Object.keys(this.entitiesPool);
+    this.actors = [];
   }
 
-  get actor() {
+  initialise() {
+  }
+
+  addNewActor() {
   }
 
   randomType() {
-    const types = Object.keys(this.entities);
+    let index = this.randomIndex(this.entityTypes.length);
 
-    // get random index for types array
-    let randomIndex = Math.round(Math.random() * (types.length - 1));
-
-    return types[randomIndex];
+    return this.entityTypes[index];
   }
 
+  randomActor() {
+    let type = this.randomType();
+    let array = this.entitiesPool[type];
+    let index = this.randomIndex(array.length);
 
+    return array[index];
+  }
+
+  randomIndex(limit) {
+    // get random index from 0 to limit-1
+    return Math.round(Math.random() * (limit - 1));
+  }
+}
+
+entitiesPool = {test1: [1, 2, 3, 4, 6],
+                test2: [12, 43, 32, 54],
+                test3: [321, 432, 543, 365]}
+
+e = new Environment(entitiesPool);
+console.log(e.randomActor())
+console.log(e.randomActor())
+console.log(e.randomActor())
+console.log(e.randomActor())
 
     
     
